f809b3e00bead9e848860f57fa0d44e01f9f179e
Camila Akhmetova <k.akhmetova.stc@innopolis.university>
2020-04-06 11:31:57 +0500

    7.8.9

---
+++ HomeWork8/HomeWork8Task4/src/homework8task4/Conversion.java
@@ -0,0 +1,105 @@
+package homework8task4;
+import java.util.Scanner;
+
+public class Conversion {
+    public static class Act{           //Класс актов
+        protected static int actNumber = 0;  //Номер акта
+
+        public Act(int day, int mounth, int year){  //Конструктор, параметры - дата акта, в конструкторе считается номер акта
+            this.day = day;
+            this.mounth = mounth;
+            this.year = year;
+            actNumber++;
+        }
+        public int getActNumber(){
+            return actNumber;
+        }
+        protected int day, mounth, year;
+        public int getDay() {
+            return (this.day);
+        }
+        public int getMounth() {
+            return (this.mounth);
+        }
+        public int getYear() {
+            return (this.year);
+        }
+
+        public String[] productsList;   //Список товаров
+        public void addList(String s){     //Метод для заполнения списка
+            productsList[productsList.length] = s;
+        }
+    }
+
+    public static class Contract{       //Класс договоров
+        protected static int contractNumber = 0;   //Номер договора
+        protected static int listNumber = 0;
+
+        public Contract(int day, int mounth, int year){
+            this.day = day;
+            this.mounth = mounth;
+            this.year = year;
+            contractNumber++;
+        }
+        public int getContractNumber(){
+            return contractNumber;
+        }
+        protected int day, mounth, year;
+        public int getDay() {
+            return (this.day);
+        }
+        public int getMounth() {
+            return (this.mounth);
+        }
+        public int getYear() {
+            return (this.year);
+        }
+
+        public String[] productsList;
+        public void addList(String s){
+            productsList[listNumber] = s;
+            listNumber++;
+        }
+
+    }
+
+    static Act conversion(Contract contr){   //метод для конвертирования договора в акт
+        Act newAct= new Act(contr.day,contr.mounth,contr.year);
+/*        for (int i=0; i<contr.productsList.length; i++){
+            newAct.productsList[i] = contr.productsList[i];
+        }
+
+ */
+        return newAct;
+    }
+
+    public static void main(String[] args){
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите поочередно день, месяц и год создания договора");
+        Contract myContract = new Contract(scn.nextInt(), scn.nextInt(), scn.nextInt());
+        System.out.println("Договор создан");
+
+/*        System.out.println("Заполните список товаров, вводите названия по одному, когда товары закончатся, введите end");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        for (int i = 0; i<10; i++){
+            myContract.addList(str);
+            str = in.nextLine();
+        }
+
+        /*while (!(str.equals("end"))){
+            Scanner scan = new Scanner(System.in);
+            myContract.addList(str);
+            str = scan.nextLine();
+        }
+
+         */
+
+        Act myAct = conversion(myContract);
+        //System.out.println(str);
+        System.out.println("Номер акта: " + myAct.getActNumber());
+        System.out.println("Дата акта: " + myAct.getDay() + "." + myAct.getMounth() + "." + myAct.getYear());
+    }
+
+}
