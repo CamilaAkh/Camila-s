e28960646e906840799acb59cc39279b56a70daf
Camila Akhmetova <k.akhmetova.stc@innopolis.university>
2020-06-05 22:14:05 +0500

    05.06

--- HomeWork/src/library/Main.java
+++ HomeWork/src/library/Main.java
@@ -6,14 +6,15 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    
     public static void main(String[] args) {
-        //Library lib = new Library();
+
         Library lib = null;
+
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(Library.class);
             Unmarshaller un = jaxbContext.createUnmarshaller();
@@ -22,46 +23,19 @@ public class Main {
         } catch (JAXBException e) {
             e.printStackTrace();
         }
-        //Book [] myLibrary = new Book[100];
-        int booksQuantity = 0;
-        Book tempBook;
-        System.out.println("Выберите действие:\n0.Выйти из библиотеки\n1.Добавить книгу\n2.Посмотреть список книг" );
-        Scanner scn = new Scanner(System.in);
-        int actionChoice = scn.nextInt();
-        //int bookchoice;
-        while (actionChoice!=0) {
-            switch (actionChoice) {
-                case 1:
-                    System.out.println("Укажите последовательно через клавишу ввода название, автора и год издания книги");
-                    try {
-                        tempBook = new Book();
-                        tempBook.setName(scn.next());
-                        tempBook.setAutor(scn.next());
-                        tempBook.setYear(scn.nextInt());
-                        lib.books.add(tempBook);
-                        //myLibrary[booksQuantity] = new Book(scn.next(), scn.next(), scn.nextInt());
-                        booksQuantity++;
-                    } catch (InputMismatchException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 2:
-                    for (Book b : lib.books) {
-                        System.out.println("'" + b.getName() +"' автор: " + b.getAutor() + " год: " + b.getYear());
 
-                    }
-                    System.out.println();
-                    break;
-                /**case 3:
-                    System.out.println("Укажите номер книги");
-                    bookchoice = scn.nextInt();
-                    System.out.printf("Книга '%s' автора %s %d года" + myLibrary[bookchoice-1].name + myLibrary[bookchoice-1].autor + myLibrary[bookchoice-1].year);
-                    System.out.println();
-                 */
-            }
-            System.out.println("Выберите действие:\n0.Выйти из библиотеки\n1.Добавить книгу\n2.Посмотреть список книг" );
-            actionChoice = scn.nextInt();
-        }
+        visitLibrary(lib);
+
+        serialization(lib);
+
+    }
+
+
+    /**
+     * метод сериализации xml-файла
+     * @param lib
+     */
+    private static void serialization(Library lib){
         if (lib != null) {
             try {
                 JAXBContext context = JAXBContext.newInstance(Library.class);
@@ -69,9 +43,56 @@ public class Main {
                 marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
                 marshaller.marshal(lib, new File("lib.xml"));
-            } catch ( JAXBException e) {
+            } catch (JAXBException e) {
                 e.printStackTrace();
             }
         }
     }
+
+
+    /**
+     * метод для работы с библиотекой(просмотр списка книг, добавление книги в список)
+     * @param lib
+     */
+    private static void visitLibrary(Library lib){
+        Book tempBook;
+
+        System.out.println("Выберите действие:\n0.Выйти из библиотеки\n1.Добавить книгу\n2.Посмотреть список книг");
+        Scanner scn = new Scanner(System.in);
+
+        try {
+
+            int actionChoice = scn.nextInt();
+
+            while (actionChoice != 0) {
+
+                switch (actionChoice) {
+                    case 1:
+                        System.out.println("Укажите последовательно через клавишу ввода название, автора и год издания книги");
+
+                        tempBook = new Book();
+                        tempBook.setName(scn.next());
+                        tempBook.setAutor(scn.next());
+                        tempBook.setYear(scn.nextInt());
+
+                        lib.books.add(tempBook);
+
+                        break;
+                    case 2:
+                        for (Book b : lib.books) {
+                            System.out.println("'" + b.getName() + "' автор: " + b.getAutor() + " год: " + b.getYear());
+
+                        }
+                        System.out.println();
+                        break;
+                }
+                System.out.println("Выберите действие:\n0.Выйти из библиотеки\n1.Добавить книгу\n2.Посмотреть список книг");
+                actionChoice = scn.nextInt();
+            }
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
